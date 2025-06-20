f7b9315d573c2f1f9711528e7a916cecd48d94f4
Emeyas Franco <emeyasdev@gmail.com>
2025-06-20 08:52:29 -0300

    conclusão exercícios JS

---
+++ JAVASCRIPT/1 - Parte 1/EXERCÍCIOS DIVERSOS/13 - exercicio.js
@@ -0,0 +1,16 @@
+// 18 -> Crie uma função simples chamada greet que imprima "Hello, World!" no console quando chamada. Chame a função greet.
+
+function greet() {
+  console.log(`Hello, World!`)
+}
+
+greet()
+greet()
+
+// 19 -> Explique o que é uma "função" no contexto da programação e por que ela é útil.
+
+// FUNCTION OU FUNÇÃO  é muito útil, pois podemos definir um bloco de código e esse bloco de código só é executado quando solicitamos/chamamos o nome da função.
+
+// 20 -> Qual a diferença entre let e const ao declarar variáveis? Quando você escolheria um em vez do outro?
+
+// Quando declaramos let podemos alterar a qualquer momento o valor da variável, já ao declarar const é uma constante o que não possibilita a alteração do valor a qualquer momento. Utilizamos sempre const, a não ser que iremos alterar o valor.
