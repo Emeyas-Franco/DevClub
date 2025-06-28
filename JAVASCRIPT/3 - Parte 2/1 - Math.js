61ae809e77b668a9801063ce30ca69c66ea06d6f
Emeyas Franco <emeyasdev@gmail.com>
2025-06-28 15:28:48 -0300

    Inicio Javascript parte 2 + projeto Sorteador

---
+++ JAVASCRIPT/3 - Parte 2/1 - Math.js
@@ -0,0 +1,25 @@
+/*
+
+Math -> Matemática
+- pow(2,2) / potência / 2² = 4
+- sqrt(25,2) / raiz / 
+- PI 
+- ceil / teto -> Arredondar pra cima
+- floor / chão -> Arredondar pra baixo
+
+-random() / número aleatório entre 0 e 1
+
+*/
+
+const result = Math.pow(5,3)
+const result2 = Math.sqrt(25,3)
+const pi = Math.PI.toFixed(4)
+const arredondar = Math.ceil(3.14)
+const arredondar2 = Math.floor(3.95)
+const random = Math.random()
+console.log(result)
+console.log(result2)
+console.log(pi)
+console.log(arredondar)
+console.log(arredondar2)
+console.log(random)
