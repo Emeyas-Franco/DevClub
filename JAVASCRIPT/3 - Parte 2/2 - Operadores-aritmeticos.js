e8f268a0848b8ddf99dc5a240236d53897a62665
Emeyas Franco <emeyasdev@gmail.com>
2025-06-29 08:43:05 -0300

    estudos dia 28-06

---
+++ JAVASCRIPT/3 - Parte 2/2 - Operadores-aritmeticos.js
@@ -0,0 +1,30 @@
+/* 
+
+Operadores Aritméticos
+  + -> Adição
+  - -> Subtração
+  / -> Divisão
+  * -> Multiplicação
+  % -> Resto da Divisão
+  ++ -> Incremento
+  -- -> Decremento
+  ** -> Exponencial
+
+*/
+let adicao = 2 + 5
+const subtracao = 2 - 5
+const divisao = 25 / 5
+const multiplicacao = 5 * 5
+const resto = 21 % 2
+const incremento = ++adicao
+const decremento = --adicao
+const exponencial = 5 ** 5 
+
+console.log(adicao)
+console.log(subtracao)
+console.log(divisao)
+console.log(multiplicacao)
+console.log(resto)
+console.log(incremento)
+console.log(decremento)
+console.log(exponencial)
